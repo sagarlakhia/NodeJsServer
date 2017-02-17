56c13f3e6a739be6a59823106358a0c9a7601c59
lakhiasagar <sagar.lakhia@mavs.uta.edu>
2017-02-17 11:10:53 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var jsonParser = bodyParser.json();
+
+mongoose.connect('mongodb://testuser:testuser@ds051553.mlab.com:51553/listusers');
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+  profession: String
+});
+
+var User = mongoose.model('User', userSchema);
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+var data = {
+  name:'sagar'
+}
+app.get('/get-users', function(req,res)
+{
+  User.find({}, function(err,data){
+        if (err)
+        {
+          res.send("");
+        }
+        else
+        {
+            res.send(data);
+        }
+      });
+});
+
+app.post('/submit-user-data', urlencodedParser, function(req,res)
+{
+  var user = User(req.body).save(function(err,data){
+    if(err) throw err;
+    res.json(data);
+  })
+});
+app.listen(3000);
